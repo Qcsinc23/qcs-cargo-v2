7848ede5a4657a47b972c045419f252c1b32ca16
Qcsinc23 <sales@quietcraftsolutions.com>
2025-12-08 01:45:28 -0500

    feat: Add SvelteKit routes and layouts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import '../app.css';
+  import { Toaster } from 'svelte-sonner';
+  import { SkipLink, OfflineIndicator } from '$lib/components/layout';
+
+  export let data;
+</script>
+
+<SkipLink />
+
+<slot />
+
+<OfflineIndicator />
+<Toaster position="top-right" richColors />
